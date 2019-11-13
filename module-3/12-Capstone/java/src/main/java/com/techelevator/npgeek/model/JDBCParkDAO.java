d527566947824c4e54e12c3455deb69e755c7c60
Dan Podobnik <daniel.podobnik@gmail.com>
2019-11-13 11:41:01 -0500

    Forgot to autowire the JDBCParkDAO

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/model/JDBCParkDAO.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/model/JDBCParkDAO.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JDBCParkDAO implements ParkDAO {
 	private JdbcTemplate jdbcTemplate;
 	
+	@Autowired
 	public JDBCParkDAO(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
