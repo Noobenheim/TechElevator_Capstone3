45733118907d2b29f6455f33fb78c51cafe2b7f2
Dan Podobnik <daniel.podobnik@gmail.com>
2019-11-14 10:56:11 -0500

    Added endpoints to controller with validation, added SurveySummary object

--- module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/NpGeekController.java
+++ module-3/12-Capstone/java/src/main/java/com/techelevator/npgeek/NpGeekController.java
@@ -1,14 +1,19 @@
 package com.techelevator.npgeek;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.techelevator.npgeek.model.ForecastDAO;
 import com.techelevator.npgeek.model.ParkDAO;
@@ -21,6 +26,9 @@ public class NpGeekController {
 	
 	@Autowired
 	private ForecastDAO forecastDAO;
+	
+	@Autowired
+	private SurveyDAO surveyDAO;
 	
 	@RequestMapping("/") 
 	public String displayHomePage(ModelMap map){
@@ -48,5 +56,29 @@ public class NpGeekController {
 		map.put("oppositeTemperature", tempInF ? "Celsius" : "Fahrenheit");
 		
 		return "details";
+	}
+	
+	@RequestMapping("/survey")
+	public String displaySurveyForm() {
+		return "surveyForm";
+	}
+	
+	@RequestMapping(path="/survey", method=RequestMethod.POST)
+	public String submitSurvey(@Valid @ModelAttribute("survey") Survey survey,
+							   BindingResult result, 
+							   RedirectAttributes flashScope) {
+		if( result.hasErrors() ) {
+			flashScope.addFlashAttribute("survey", survey);
+			flashScope.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX+"survey", result);
+			
+			return "redirect:/survey";
+		}
+		
+		
+	}
+	
+	@RequestMapping("/favoriteParks")
+	public String displayFavoriteParks() {
+		return "favoriteParks";
 	}
 }
